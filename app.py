c586e0514d279a30c7f2912548ba590f4d22ffce
Mike Roth <mikerothart@gmail.com>
2022-08-25 16:27:46 -0400

    init commit

---
+++ app.py
@@ -0,0 +1,96 @@
+import streamlit as st
+import streamlit.components.v1 as components
+from src.LatentSpace import LatentSpaceApp
+import umap
+from joblib import load
+import matplotlib.pyplot as plt
+import pandas as pd
+import plotly.express as px
+
+
+def search(query):
+	st.experimental_set_query_params(query=query)
+	with st.spinner('Loading Tracks...'):
+		autoencoder_path = 'data/autoencoder_256dim_time_freq_128k_20epochs'
+		latent_space = LatentSpaceApp(autoencoder_path, latent_dims=256, num_tiles=64)
+		latent_space.load(autoencoder_path)
+		tab1, tab2 = st.tabs(['Song Recommendations', 'Latent Space Visualization'])
+	try:
+		with tab1:
+			track, df, latents, this_track= latent_space.search_for_recommendations(query, get_time_and_freq=True)
+			display_song('This Song', track_id=track['id'], track_name=track['name'], artist=track["artists"][0]["name"])
+
+			for idx, row in df.iterrows():
+				track_id = row.track_uri.split(':')[-1]
+				display_song(idx+1, track_id=track_id, track_name=row.track_name, artist=row.artist_name, similarity=row.similarity, time_similarity=row.time_similarity, freq_similarity=row.frequency_similarity)
+		with tab2:
+			fig = plot_genre_space(track, this_track, latents, latent_space)
+			st.header('Genres in the Sonic Landscape')
+			st.subheader(track['name'] + ' by ' + track['artists'][0]['name'])
+			st.plotly_chart(fig, use_container_width=True)
+			st.write('Using dimensionality reduction, basic genres and similar songs can be plotted in a visualization of the latent space that is created by an encoder.')
+
+
+	except:
+		st.write("""No preview for this song or artist on Spotify. Please try a different search.
+			This recommendation system relies on the preview mp3's provided by Spotify's Public API.
+			If there is no mp3 preview, Sonufy can't make a recommendation.""")
+
+def display_song(index, track_id, track_name, artist, similarity=None, time_similarity=None, freq_similarity=None):
+	col1, col2, col3 = st.columns((1,3,3))
+	if str(index).isnumeric():
+		if index % 2 == 1:
+			middle = col2
+			end = col3
+		else:
+			middle = col3
+			end = col2
+	else:
+		middle = col3
+		end = col2
+
+	with col1:
+		st.header(index)
+	with middle:
+		st.subheader(f'{track_name} by {artist}')
+		if similarity != None:
+			st.write(f'Similarity: {round(similarity,2)}')
+		if time_similarity != None:
+			st.write(f'Time Similarity: {round(time_similarity,2)}')
+		if freq_similarity != None:
+			st.write(f'Frequency Similarity: {round(freq_similarity,2)}')
+	with end:
+		components.iframe(f'https://open.spotify.com/embed/track/{track_id}', width=250, height=250)
+
+def plot_genre_space(track, this_track, latents, latent_space):
+	this_track_df = pd.DataFrame(this_track, columns=latent_space.latent_cols)
+	this_track_df['name'] = track['name'] + ' - ' + track['artists'][0]['name']
+	this_track_df['label'] = 2
+
+	latents['name'] = latents['track_name'] + ' - ' + latents['artist_name']
+	latents['label'] = 1
+	latents = latents[['name'] + latent_space.latent_cols + ['label']]
+
+	genres_and_tracks = pd.concat([latent_space.genres, latents, this_track_df]).reset_index(drop=True)
+	genre_map = load('data/autoencoder_256dim_time_freq_128k_20epochs/genre_map.bin')
+	genre_map_trans = genre_map.transform(genres_and_tracks[latent_space.latent_cols])
+
+	genre_map_df = pd.DataFrame(genre_map_trans, columns=['x','y'])
+	genre_map_df = pd.concat([genres_and_tracks[['name','label']], genre_map_df], axis=1)
+	genre_map_df.label = genre_map_df.label.map({0:'genre', 1:'similar song', 2:'this song'})
+	genre_map_df['annotation'] = genre_map_df.apply(lambda x: x['name'] if x['label'] == 'genre' else '', axis=1)
+
+	fig = px.scatter(genre_map_df, x='x', y='y', color='label', hover_name='name', size=[.5]*len(genre_map_df), width=800, height=600, text='annotation')
+	return fig		
+try:
+	params = st.experimental_get_query_params()
+	params_query = params['query'][0]
+except:
+	params_query = ''
+st.header('Sonufy')
+query = st.text_input('search for a track on Spotify to hear similar songs:', value='')
+if query != '':
+	search(query)
+elif params_query != '':
+	search(params_query)
+
---
+++ src/LatentSpace.py
@@ -0,0 +1,140 @@
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from pyarrow import feather
+import time
+from sklearn.metrics.pairwise import cosine_similarity, euclidean_distances
+from sklearn.preprocessing import StandardScaler
+from src.DataGenerator import AudioDataGenerator
+from src.helper_functions import progress_bar
+from joblib import dump, load
+import json
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+
+
+
+class LatentSpaceApp:
+    
+    def __init__(self,
+                autoencoder_path,
+                image_dir='data/Spotify/comp_pngs',
+                sample_size=None,
+                latent_dims=128,
+                num_channels=1,
+                output_size=(128,128),
+                scale=True,
+                threshold_level=0,
+                num_tiles=4):
+        self._batch_size = 1
+        self.autoencoder = tf.keras.models.load_model(autoencoder_path)
+        self.prediction_generator = AudioDataGenerator(directory=image_dir,
+                                    image_size=(128,512),
+                                    color_mode='rgb',
+                                    batch_size=1, 
+                                    shuffle=False,
+                                    sample_size = sample_size,
+                                    output_channel_index=0,
+                                    num_output_channels=num_channels,
+                                    output_size=output_size,
+                                    threshold_level=threshold_level)
+        self.latent_cols = [f'latent_{i}' for i in range(latent_dims)]
+        
+        self.size = self.prediction_generator.size
+        self._num_channels = num_channels
+        self._scale = scale
+        self._num_tiles = num_tiles
+
+        f = open('data/apikeys/.apikeys.json')
+        apikeys = json.load(f)
+        client_id = apikeys['clientId']
+        client_secret = apikeys['clientSecret']
+
+        credentials_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
+
+        self._spotify = spotipy.Spotify(client_credentials_manager=credentials_manager)
+        
+    
+    def load(self, directory_to_load, load_full_results=False):
+        try:
+            self.tracks = feather.read_feather(directory_to_load+'/tracks.feather')
+            print('Loaded tracks.')
+        except:
+            print('Failed to load tracks.')
+
+        try:
+            self.genres = feather.read_feather(directory_to_load+'/genres.feather')
+            print('Loaded genres.')
+        except:
+            print('Failed to load genres.')
+
+        try:
+            self._scaler=load(directory_to_load+'/std_scaler.bin')
+            print('loaded scaler')
+        except:
+            print('Failed to load scaler.')
+
+        try:
+            self._genre_map = load(directory_to_load+'/genre_map.bin')
+            print('loaded genre map')
+        except:
+            print('Failed to load genre map')
+
+        
+
+    def get_similarity(self, df1, df2, subset, num=10, similarity_measure='cosine', popularity_threshold=0, sort_tracks=True):
+        if similarity_measure == 'cosine':
+            similarity_measure_fn = cosine_similarity
+            sort = False
+        elif similarity_measure == 'euclidean':
+            similarity_measure_fn = euclidean_distances
+            sort = True
+        else:
+            raise ValueError('similarity_measure must be "cosine" or "euclidean"')
+
+        similarity = similarity_measure_fn(np.array(df1[subset]), np.array(df2[subset]))
+
+        similarity_df = df2.copy()
+        similarity_df['similarity'] = similarity.T
+
+        if popularity_threshold > 0:
+            similarity_df = similarity_df[similarity_df.track_popularity > popularity_threshold]
+
+        if sort_tracks:
+            similarity_df = similarity_df.sort_values(by='similarity', ascending=sort).reset_index()
+
+        return similarity_df[:num]
+
+
+    def get_vector_from_preview_link(self, link, track_id):
+        img = self.prediction_generator.get_vector_from_preview_link(link, track_id, num_tiles=self._num_tiles)
+        vector = np.array(self.autoencoder.encoder(img[0])).mean(axis=0)
+        vector = self._scaler.transform(pd.DataFrame([vector], columns=self.latent_cols))
+        vector = pd.DataFrame(vector, columns=self.latent_cols)
+        return vector
+
+    def search_for_recommendations(self, query, num=10, popularity_threshold=10, get_time_and_freq=False):
+        id_ = self._spotify.search(query, type='track')['tracks']['items'][0]['id']
+        track = self._spotify.track(id_)
+        link = track['preview_url']
+        print(track['name'])
+        print(track['artists'][0]['name'])
+        print(link)
+
+        if link is not None:
+
+            vector = self.get_vector_from_preview_link(link, id_)
+            similarity = self.get_similarity(vector, self.tracks, subset=self.latent_cols, num=50, popularity_threshold=popularity_threshold)
+
+            similarity = similarity[~similarity.track_name.apply(lambda x: x.lower()).isin([track['name'].lower()])][:num].reset_index()
+
+            if get_time_and_freq:
+                similarity['time_similarity'] = self.get_similarity(vector, similarity, subset=self.latent_cols[:len(self.latent_cols)//2], num=num, popularity_threshold=popularity_threshold, sort_tracks=False)['similarity']
+                similarity['frequency_similarity'] = self.get_similarity(vector, similarity, subset=self.latent_cols[len(self.latent_cols)//2:], num=num, popularity_threshold=popularity_threshold, sort_tracks=False)['similarity']
+            
+                return track, similarity[['track_name','track_uri','artist_name','similarity','track_popularity','time_similarity','frequency_similarity']], similarity, vector
+            else:
+                return track, similarity[['track_name','track_uri','artist_name','similarity','track_popularity']], similarity, vector
+        else:
+            print('No Preview Available. Try a different search.')
