9c4378f60470914f5f0e2c2277e8295abfa4ae10
Mike Roth <mikerothart@gmail.com>
2022-08-27 13:27:05 -0400

    added steup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
 mkdir -p ~/.streamlit/
-echo "
-[general]n
-email = "mikerothart@gmail.com"n
+echo "\
+[general]\n\
+email = \"mikerothart@gmail.com\"\n\
 " > ~/.streamlit/credentials.toml
-echo "
-[server]n
-headless = truen
-enableCORS=falsen
-port = $PORTn
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
 " > ~/.streamlit/config.toml
