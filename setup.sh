7c9fc52292ed1a94151c8916631a1f5c3b992bdc
Mike Roth <mikerothart@gmail.com>
2022-09-06 15:03:28 -0400

    added secrets to setup

--- setup.sh
+++ setup.sh
@@ -10,7 +10,7 @@ enableCORS=false\n\
 port = $PORT\n\
 " > ~/.streamlit/config.toml
 
-echo"\
+echo "\
 clientId = '5f818fbc1c374e239ee0139163734165'\n\
 clientSecret = 'dd3e1522369f4413bfadc8613cf7bf43'\n\
 " > ~/.streamlit/secrets.toml
