dca08b333e05dc8cca7cb38d3bc9d1d26d5b00a4
Mike Roth <mikerothart@gmail.com>
2022-09-07 00:32:40 -0400

    added progress bar

--- app.py
+++ app.py
@@ -19,16 +19,21 @@ def load_sonufy():
 
 def search(query):
 	st.experimental_set_query_params(query=query)
-	with st.spinner('Loading Tracks...'):
+	status = st.empty()
+	with status.container():
+		progress_bar = st.progress(0)
+		st.write('Loading Tracks...')
 
-		sonufy = load_sonufy()
+	sonufy = load_sonufy()
+	progress_bar.progress(10)
 
-		tab1, tab2 = st.tabs(['Song Recommendations', 'Latent Space Visualization'])
-		# try:
+	tab1, tab2 = st.tabs(['Song Recommendations', 'Latent Space Visualization'])
+	try:
 		with tab1:
 			track, df, latents, this_track= sonufy.search_for_recommendations(query, get_time_and_freq=True)
+			progress_bar.progress(50)
 			display_song('This Song', track_id=track['id'], track_name=track['name'], artist=track["artists"][0]["name"])
-
+			progress_bar.progress(75)
 			for idx, row in df.iterrows():
 				track_id = row.track_uri.split(':')[-1]
 				display_song(idx+1, 
@@ -38,6 +43,7 @@ def search(query):
 							similarity=row.similarity,
 							time_similarity=row.time_similarity,
 							freq_similarity=row.frequency_similarity)
+			progress_bar.progress(100)
 
 		with tab2:
 			# fig = plot_genre_space(track, this_track, latents, latent_space)
@@ -47,10 +53,10 @@ def search(query):
 			st.write('Using dimensionality reduction, basic genres and similar songs can be plotted in a visualization of the latent space that is created by an encoder.')
 
 
-		# except:
-		# 	st.write("""No preview for this song or artist on Spotify. Please try a different search.
-		# 		This recommendation system relies on the preview mp3's provided by Spotify's Public API.
-		# 		If there is no mp3 preview, Sonufy can't make a recommendation.""")
+	except:
+		st.warning("""No preview for this song or artist on Spotify. Please try a different search.
+			This recommendation system relies on the preview mp3's provided by Spotify's Public API.
+			If there is no mp3 preview, Sonufy can't make a recommendation.""")
 
 def display_song(index, track_id, track_name, artist, similarity=None, time_similarity=None, freq_similarity=None):
 	col1, col2, col3 = st.columns((1,3,3))
@@ -83,7 +89,8 @@ try:
 	params_query = params['query'][0]
 except:
 	params_query = ''
-st.header('Sonufy')
+# st.header('Sonufy')
+st.image('data/img_working/sonufy_logo.png')
 query = st.text_input('search for a track on Spotify to hear similar songs:', value='')
 if query != '':
 	search(query)
