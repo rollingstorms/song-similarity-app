30b2c03e2fc0de59a8d695fe4b93c89ba0eec8e8
Mike Roth <mikerothart@gmail.com>
2022-09-07 21:26:11 -0400

    html experiment

--- app.py
+++ app.py
@@ -85,14 +85,14 @@ def display_song(index, track_id, track_name, artist, similarity=None, time_simi
 			st.write(f'Frequency Similarity: {round(freq_similarity,2)}')
 	with end:
 		embed_script = f"""
-		window.onSpotifyIframeApiReady = (IFrameAPI) => {{
+		<script>window.onSpotifyIframeApiReady = (IFrameAPI) => {{
 		let element = document.getElementById('embed-iframe');
 		let options = {{
 		  uri: 'spotify:episode:{track_id}'
 		}};
 		let callback = (EmbedController) => {{}};
 		IFrameAPI.createController(element, options, callback);
-		}};"""
+		}};</script>"""
 		st.write(embed_script, unsafe_allow_html=True)
 		# components.iframe(f'https://open.spotify.com/embed/track/{track_id}', width=250, height=250)
 
